6aa56fa0dedd79c2795446f154d59769b5163409
Shubh7Codes <shubh7codes@gmail.com>
2025-05-17 20:47:48 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits, Collection, Events } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.commands = new Collection();
+
+const folderPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(folderPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, interaction => {
+    // To make sure the command is a slash command and not a Chat Input
+    if (!interaction.isChatInputCommand()) return;
+    
+    const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+	console.log(interaction);
+});
+
+client.on("messageCreate", (message) =>{
+    if (message.author.bot) return; // Ignore bot messages
+    // console.log(message);
+    message.reply(`You Said: ${message.content}`);
+});
+
+client.login(token);
